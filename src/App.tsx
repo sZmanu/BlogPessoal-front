0a0f97deb0c6be610ecc94853b4b12d070d000d5
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-01-14 17:19:59 -0300

    NavBar e Footer criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/NavBar'
 import Home from './pages/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Home/>
-  )
+		<>
+			<Navbar />
+			<Home />
+			<Footer />
+		</>
+	)
 }
 
 export default App
