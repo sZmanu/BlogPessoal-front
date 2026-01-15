4f7b072b97c27591a5f69d8a0f69ce19130eece6
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-01-15 12:10:25 -0300

    Novas rotas adicionadas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,13 +10,19 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
+                            Blog Pessoal Manuella Oliveira | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
+                        <a href="https://www.linkedin/in/manuellaalves" target="_blank">
                         <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/manuella4252" target="_blank">
                         <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="" target="_blank">
                         <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,18 +1,34 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
+            			   bg-violet-950 text-violet-200'>
             
-                <div className="container flex justify-between text-lg mx-8">
-                    Blog Pessoal
+                <div className="container flex justify-between text-lg-min8">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-                    <div className='flex gap-4'>
+                    <div className='flex font-medium min-w-2/5 text-xl  justify-evenly'>
+                        <Link to={''}>
                         Postagens
+                        </Link>
+
+                        <Link to={''}>
                         Temas
+                        </Link>
+
+                        <Link to={''}>
                         Cadastrar tema
+                        </Link>
+
+                        <Link to={''}>
                         Perfil
+                        </Link>
+
+                        <Link to={''}>
                         Sair
+                        </Link>
                     </div>
                 </div>
             </div>
