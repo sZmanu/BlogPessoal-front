3a7107e73cbe7c1630007b04be192a1897203722
sZmanu <manuellaalvesdeoliveira62@gmail.com>
2026-01-22 18:17:10 -0300

    Deploy do projeto

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-2upb.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 //as services fazem a comunicação com o back, enviando as requisições e trazendo as respostas
